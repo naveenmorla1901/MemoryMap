19c4a51ba5a34f70d9f8573b11980227807f3d8a
naveenmorla1901 <morla.naveen1901@gmail.com>
2024-11-24 20:36:20 -0500

    sunday eve

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,12 @@
 // metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
-const config = getDefaultConfig(__dirname, {
-  isCSSEnabled: true,
-});
+const config = getDefaultConfig(__dirname);
 
-// Modify the config
-config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json', 'cjs', 'mjs'];
-config.resolver.assetExts = ['png', 'jpg', 'jpeg', 'gif', 'svg'];
-
-// Add additional module directories
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
-];
-
-module.exports = config;
+module.exports = {
+  ...config,
+  resolver: {
+    ...config.resolver,
+    sourceExts: [...config.resolver.sourceExts, 'mjs', 'cjs'],
+  },
+};
